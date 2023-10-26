5300a66ed76c31392c81b89a52e672fd39ebb036
OceanSkys <90655578+OceanSkys@users.noreply.github.com>
2023-10-25 19:00:57 -0700

    hopefully fixed height/width mobile and desktop friendly issues with contact form

--- newport/src/components/Contact.js
+++ newport/src/components/Contact.js
@@ -11,7 +11,7 @@ return (
         <div class="row row-form">
           <div class="col d-flex justify-content-center form-col">
             <div class="row d-flex flex-column follow-me">
-              <div style={{border: 'solid', borderColor:'pink'}}class="col p-2 d-flex pic-col justify-content-center align-content-center">
+              <div class="col p-2 d-flex pic-col justify-content-center align-content-center">
                 <img src={PicofMe} class=" d-flex align-content-center justify-content-center aboutmepic2 " alt="Picture of Jeremy"/> 
               </div>
               <div class="col d-flex email-col justify-content-center align-items-center">
@@ -28,10 +28,10 @@ return (
                 <p style={{fontSize: '50px'}}>Follow me at:</p>
               </div>
               <div class="col d-flex justify-content-center align-items-center">
-                <a href="https://github.com/OceanSkys" target="_blank"><FontAwesomeIcon icon={faGithub} className="p-1 fa-5x" style={{textDecoration: 'none', color: 'black', height:'50px', width:'50px'}}/></a>
-                <a href="https://www.linkedin.com/in/jeremy-thornton-7aa1b9248/" target="_blank"><FontAwesomeIcon icon={faLinkedin} className="p-1 fa-5x" style={{textDecoration: 'none', color: '0077b5', height:'50px', width:'50px'}}/></a>
-                <a href="https://www.instagram.com/jeremyyzz/" target="_blank"><FontAwesomeIcon icon={faInstagram} className="p-1 fa-5x" style={{textDecoration: 'none', color: 'red', height:'50px', width:'50px'}}/></a>
-                <a href="https://www.facebook.com/profile.php?id=100009856054876" target="_blank"><FontAwesomeIcon icon={faFacebook} className="p-1 fa-5x" style={{textDecoration: 'none', color: 'blue', height:'50px', width:'50px'}}/></a>
+                <a href="https://github.com/OceanSkys" target="_blank"><FontAwesomeIcon icon={faGithub} className="p-1 fa-3x" style={{textDecoration: 'none', color: 'black', height:'50px', width:'50px'}}/></a>
+                <a href="https://www.linkedin.com/in/jeremy-thornton-7aa1b9248/" target="_blank"><FontAwesomeIcon icon={faLinkedin} className="p-1 fa-3x" style={{textDecoration: 'none', color: '0077b5', height:'50px', width:'50px'}}/></a>
+                <a href="https://www.instagram.com/jeremyyzz/" target="_blank"><FontAwesomeIcon icon={faInstagram} className="p-1 fa-3x" style={{textDecoration: 'none', color: 'red', height:'50px', width:'50px'}}/></a>
+                <a href="https://www.facebook.com/profile.php?id=100009856054876" target="_blank"><FontAwesomeIcon icon={faFacebook} className="p-1 fa-3x" style={{textDecoration: 'none', color: 'blue', height:'50px', width:'50px'}}/></a>
               </div>
             </div>
           </div>
